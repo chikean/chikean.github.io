c9514448806fb4abecac6484931c80f213f77dac
chikean <chikean@users.noreply.github.com>
2018-01-23 21:22:40 +1100

    Remove Azure function

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,86 +52,86 @@ $( document ).ready(function() {
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var debug = true;
-    if (!debug) {
-        $('#thesis-count').removeClass("blink");
-        $('#thesis-count').text(sum);
-    } else {
-        $.ajax({
-            url: "https://function-cchow-1.azurewebsites.net/api/QueryCountWebhook?code=KKkpQXBqL9MUTFEiYcpSXJXqmQmNnusC7D9oTxpYU94Zg04QkenNAg==&clientId=default",
-            type: "GET",
-            beforeSend: function(xhr){xhr.setRequestHeader('Content-Type', 'application/json');},
-            success: function(data) { 
+    // if (!debug) {
+    //     $('#thesis-count').removeClass("blink");
+    //     $('#thesis-count').text(sum);
+    // } else {
+    //     $.ajax({
+    //         url: "https://function-cchow-1.azurewebsites.net/api/QueryCountWebhook?code=KKkpQXBqL9MUTFEiYcpSXJXqmQmNnusC7D9oTxpYU94Zg04QkenNAg==&clientId=default",
+    //         type: "GET",
+    //         beforeSend: function(xhr){xhr.setRequestHeader('Content-Type', 'application/json');},
+    //         success: function(data) { 
                 
-                var cumulative = data.reduce(function(d, i) {
-                    d.push((d.length && d[d.length -1] || 0) + i.net);
-                    return d;
-                }, []);
+    //             var cumulative = data.reduce(function(d, i) {
+    //                 d.push((d.length && d[d.length -1] || 0) + i.net);
+    //                 return d;
+    //             }, []);
 
-                data.forEach(function(d, i) {
-                    d.time = parseTime(d.time);
-                    d.net = +d.net;
-                    d.cumulative = cumulative[i];
-                });
+    //             data.forEach(function(d, i) {
+    //                 d.time = parseTime(d.time);
+    //                 d.net = +d.net;
+    //                 d.cumulative = cumulative[i];
+    //             });
 
-                x.domain(d3.extent(data, function(d) {return d.time; }));
-                y.domain(d3.extent(data, function(d) {return d.cumulative; }));
+    //             x.domain(d3.extent(data, function(d) {return d.time; }));
+    //             y.domain(d3.extent(data, function(d) {return d.cumulative; }));
 
-                // Add the X Axis
-                svg.append("g")
-                    .attr("transform", "translate(0," + height + ")")
-                    .call(d3.axisBottom(x).ticks(5).tickFormat(d3.timeFormat("%b %y")));
+    //             // Add the X Axis
+    //             svg.append("g")
+    //                 .attr("transform", "translate(0," + height + ")")
+    //                 .call(d3.axisBottom(x).ticks(5).tickFormat(d3.timeFormat("%b %y")));
 
-                // Add the Y Axis
-                svg.append("g")
-                    .call(d3.axisLeft(y));
+    //             // Add the Y Axis
+    //             svg.append("g")
+    //                 .call(d3.axisLeft(y));
                 
-                var valueline = d3.line()
-                    .x(function(d) { return x(d.time); })
-                    .y(function(d) { return y(d.cumulative); });
+    //             var valueline = d3.line()
+    //                 .x(function(d) { return x(d.time); })
+    //                 .y(function(d) { return y(d.cumulative); });
 
-                // Add the path
-                svg.append("path")
-                    .attr("d", valueline(data))
-                    .attr('stroke', 'green')
-                    .attr('stroke-width', 2)
-                    .attr('fill', 'none');
+    //             // Add the path
+    //             svg.append("path")
+    //                 .attr("d", valueline(data))
+    //                 .attr('stroke', 'green')
+    //                 .attr('stroke-width', 2)
+    //                 .attr('fill', 'none');
 
-                // Add dots with tooltips
-                svg.selectAll("dot")
-                    .data(data)
-                    .enter().append("circle")
-                    .attr("r", 3)
-                    .attr("cx", function(d) { return x(d.time); })
-                    .attr("cy", function(d) { return y(d.cumulative); })
-                    .on("mouseover", function(d) {
-                        div.transition()
-                            .duration(0)
-                            .style("opacity", 1)
-                        var html = '<span class="font-weight-bold">' + d.cumulative + ' total words.</span><br>'
-                        + '<span class="text-muted">' + d.time + "</span><br>" 
-                        + '<span class="text-success">' + d.added + ' words added.</span>' + '<br>'
-                        + '<span class="text-danger">' + d.deleted + ' words removed.</span>' + '<br>'
-                        + '<span class="text-dark">' + d.net + ' net difference.</span>';
-                        div.html(html)
-                            .style("left", (d3.event.pageX + 50) + "px")
-                            .style("top", (d3.event.pageY - 28) + "px");
-                    })
-                    // .on("mouseout", function(d) {
-                    //     div.transition()
-                    //         .duration(500)
-                    //         .style("opacity", 0);
-                    // })
+    //             // Add dots with tooltips
+    //             svg.selectAll("dot")
+    //                 .data(data)
+    //                 .enter().append("circle")
+    //                 .attr("r", 3)
+    //                 .attr("cx", function(d) { return x(d.time); })
+    //                 .attr("cy", function(d) { return y(d.cumulative); })
+    //                 .on("mouseover", function(d) {
+    //                     div.transition()
+    //                         .duration(0)
+    //                         .style("opacity", 1)
+    //                     var html = '<span class="font-weight-bold">' + d.cumulative + ' total words.</span><br>'
+    //                     + '<span class="text-muted">' + d.time + "</span><br>" 
+    //                     + '<span class="text-success">' + d.added + ' words added.</span>' + '<br>'
+    //                     + '<span class="text-danger">' + d.deleted + ' words removed.</span>' + '<br>'
+    //                     + '<span class="text-dark">' + d.net + ' net difference.</span>';
+    //                     div.html(html)
+    //                         .style("left", (d3.event.pageX + 50) + "px")
+    //                         .style("top", (d3.event.pageY - 28) + "px");
+    //                 })
+    //                 // .on("mouseout", function(d) {
+    //                 //     div.transition()
+    //                 //         .duration(500)
+    //                 //         .style("opacity", 0);
+    //                 // })
 
-                // createCookie('thesis-count',sum,1);
-                // $('#thesis-count').removeClass("blink");
-                // $('#thesis-count').text(sum);
-            },
-            error: function(data) {
-                $('#thesis-count').removeClass("blink");
-                $('#thesis-count').text("Failed to connect. Please try again later.");
-            }
-        })
-    }
+    //             // createCookie('thesis-count',sum,1);
+    //             // $('#thesis-count').removeClass("blink");
+    //             // $('#thesis-count').text(sum);
+    //         },
+    //         error: function(data) {
+    //             $('#thesis-count').removeClass("blink");
+    //             $('#thesis-count').text("Failed to connect. Please try again later.");
+    //         }
+    //     })
+    //}
 
 
 });
