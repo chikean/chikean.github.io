89a779f5b25e99166e5ce9bbe1f9df34406ac287
chikean <chikean@users.noreply.github.com>
2020-12-22 13:40:21 +1100

    :sparkles: Add linkedin, github links

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-footer color="lighten-1" padless absolute>
+    <v-row justify="center">
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        :href="icon.href"
+        class="ma-4 white--text"
+        icon
+      >
+        <v-icon size="24px">
+          {{ icon.icon }}
+        </v-icon>
+      </v-btn>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: [
+      { icon: "mdi-linkedin", href: "https://www.linkedin.com/in/chris-chow/" },
+      { icon: "mdi-github", href: "https://github.com/chikean" },
+    ],
+  }),
+};
+</script>
